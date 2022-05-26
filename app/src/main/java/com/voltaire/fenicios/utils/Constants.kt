80032981c3bd6f6824829e9a517ee0cfa8cee394
Andre Esperança <andreeesperanca2010@gmail.com>
2022-05-26 18:46:43 -0300

    mpcredentials

--- app/src/main/java/com/voltaire/fenicios/utils/Constants.kt
+++ app/src/main/java/com/voltaire/fenicios/utils/Constants.kt
@@ -3,8 +3,8 @@ package com.voltaire.fenicios.utils
 class Constants {
 
     companion object {
-        const val ACCESS_SANDBOX_TOKEN = "TEST-4589497852166609-042514-bb3c4c6085b5dd3df1e947d3c3f9a29c-1001599393"
-        const val _URL = "https://api.mercadopago.com/checkout/preferences?access_token="
-        const val PUBLIC_KEY_SANDBOX = "TEST-fa840a8a-0df1-4995-830c-a81ba5b9b4d1"
+        const val ACCESS_SANDBOX_TOKEN = ""
+        const val _URL = ""
+        const val PUBLIC_KEY_SANDBOX = ""
     }
 }
